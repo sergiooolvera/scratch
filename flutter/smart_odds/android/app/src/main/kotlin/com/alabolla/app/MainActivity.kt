d46bae2c6814cf7c7731c947db14623ec81b32d1
sergiooolvera <46055574+sergiooolvera@users.noreply.github.com>
2026-06-21 14:11:05 -0600

    chore: aislar repositorio git excluyendo otros proyectos ajenos a CursosIEDCH

--- flutter/smart_odds/android/app/src/main/kotlin/com/alabolla/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.alabolla.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- peluches/src/config/site.ts
+++
@@ -1,4 +0,0 @@
-export const siteConfig = {
-  name: process.env.NEXT_PUBLIC_SITE_NAME || "Volando al Universo",
-  description: "Regalos que abrazan el alma - Peluches ultra suaves y personalizados.",
-}
